c8db71a0928847f1e399d9b1f554be0bd8bf6b82
raselASrock <mdraselm413@gmail.com>
2022-08-16 18:22:46 +0600

    deposite amount string to number

--- js/deposite.js
+++ js/deposite.js
@@ -4,12 +4,19 @@ document.getElementById('btn-deposite').addEventListener('click', function () {
     // Step: 2 get the deposite amount from the deposite input field
     // for input field use .value to get the value of an input filed
     const depositeField = document.getElementById('deposite-field');
-    const depositeAmount = depositeField.value;
-    // console.log(depositeAmount);
+    const newDepositeAmountString = depositeField.value;
+    const newDepositeAmount = parseFloat(newDepositeAmountString)
+    // console.log(typeof newDepositeAmount);
     // Step: 3 get the current deposite total
     const depositeTotalElement = document.getElementById('deposite-total');
     // for non-input( element other than input, text area) use innerText to get the text
-    const depositeTotal = depositeTotalElement.innerText;
-    // console.log(depositeTotal)
-    depositeTotalElement.innerText = depositeAmount;
+    const previousDepositeTotalString = depositeTotalElement.innerText;
+    const previousDepositeTotal = parseFloat( previousDepositeTotalString);
+    const currentDepositeTotal = previousDepositeTotal + newDepositeAmount ;
+    console.log(typeof previousDepositeTotal)
+    
+    depositeTotalElement.innerText = currentDepositeTotal;
+
+    // Step: 7 Clear the deposite field
+    depositeField.value = ''
 })
